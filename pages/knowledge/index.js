1e712913f127328234e4e081bcf5fb6b1a51b84b
Alexis DINQUER <adinquer@yahoo.com>
2021-12-18 17:42:43 +0100

    feat: Knowledge (#10)

---
+++ pages/knowledge/index.js
@@ -0,0 +1,14 @@
+import Layout from "../../components/layout";
+import Heading1 from "../../components/heading1";
+
+export default function Knowledge() {
+  return (
+    <>
+      <Layout>
+        <div className="container mx-auto px-4">
+          <Heading1>Connaissances</Heading1>
+        </div>
+      </Layout>
+    </>
+  );
+}
